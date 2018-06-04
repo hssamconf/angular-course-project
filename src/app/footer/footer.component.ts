4f80643673ce61a5c5b60e6bec44042ab6986ca7
Houssam Eddine RAFIKI <houssam-eddine.rafiki@findl.lu>
2018-06-04 11:11:17 +0000

    Accessing to dom from angular with @ViewChild

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,10 +7,14 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   @Input() title: string;
+  @ViewChild('copyrightYear') copyrightYear: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('Footer init : @ViewChild("copyrightYear") output :');
+    console.log(this.copyrightYear);
+    console.log('Value #copyrightYear : ' + this.copyrightYear.nativeElement.value);
   }
 
 }
